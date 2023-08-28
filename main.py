2c76944c3db3cdcbd6ec8562e0b31a1f33434639
JPersonalCode <daballoon@gmail.com>
2023-08-28 20:55:36 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import random
+
+# Globals
+Options = ["R", "P", "S"]
+PlayerWins = 0
+ComputerWins = 0
+
+
+# Functions
+def validateinput(string):
+    if string not in Options:
+        raise Exception("Invalid Input")
+    else:
+        return True
+
+
+def computer_input():
+    random_int = random.randrange(0, 3)
+    computer_choice = Options[random_int]
+    print("Computer Chose: " + computer_choice)
+    return computer_choice
+
+
+def compare(userinput, computer_choice):
+    if userinput == computer_choice:
+        return 0
+    elif (userinput == "R" and computer_choice == "S") or (userinput == "P" and computer_choice == "R") \
+            or (userinput == "S" and computer_choice == "P"):
+        return 1
+    else:
+        return 2
+
+
+# Call Stack
+while True:
+    player_input = input("Rock, Paper or Scissors or Q: ")
+    if player_input == "Q":
+        break
+    else:
+        validateinput(player_input)
+        result = compare(player_input, computer_input())
+    if result == 0:
+        print("Tie")
+    elif result == 1:
+        print("Player Wins!")
+        PlayerWins += 1
+        print("Player Total Wins: " + str(PlayerWins))
+        int(PlayerWins)
+    elif result == 2:
+        print("Computer Wins!")
+        ComputerWins += 1
+        print("Total Computer Wins: " + str(ComputerWins))
+        int(ComputerWins)
